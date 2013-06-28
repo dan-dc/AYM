ff490cf2866c13766af84e263954864b27afd893
dan-dc <DCStudio01@DCs-iMac-2.local>
2013-06-28 14:13:16 +0100

    Semantic rework to news, single & gallery templates. Merge stylesheets.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,58 @@
 
-$(document).ready(function() {
+/*************************************************************************
+
+	AWARDS FOR YOUNG MUSICIANS
 	
-	//alert('yep');
+	Developed by Dan Moe & Ruyman Rodriguez 
+	for Design Culture - http://www.design-culture.co.uk/ (c) 2013
+	
+	MODULE: 	styles.scss
+	PURPOSE:	main site styles
+	
+*************************************************************************/
+
+$(document).ready(function() {
+
+/*************************************************************************
+ *  Drop down navigation
+*************************************************************************/
+	$('#nav li').has('ul').addClass('drop-down') 
+	//.append('<span class="drop-down">&rsaquo;</span>')
 	
 	$('#nav li').hover(function(){
 		$(this).find('ul').stop().slideToggle();
 	})
 	
-})
+/*************************************************************************
+ *  Isotope filter
+*************************************************************************/
+
+	var $container = $('#ItemsGallery');
+	
+	$container.isotope({
+		filter: '*',
+		animationOptions: {
+		duration: 750,
+		easing: 'linear',
+		queue: false,
+		}
+		
+	});
+
+	$('#Filter a').click(function(){
+		var selector = $(this).attr('data-filter');
+		$container.isotope({ 
+			filter: selector,
+			animationOptions: {
+				duration: 750,
+				easing: 'linear',
+				queue: false,
+			}
+		});
+		return false;
+	});
+	
+	
+	
+    
+}) // end doc ready()
